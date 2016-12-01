4cb307d7d8c390f3deb2a98c208c4f7adaf30beb
Bruno Mayer <brunomayer@MacBook-Pro-de-Bruno-Mayer.local>
2016-12-01 13:53:56 -0200

    socket implemented

--- server/server.js
+++ server/server.js
@@ -1,13 +1,25 @@
 const path = require('path');
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 
 
 var publicPath = path.join(__dirname, '../public');
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
 app.use(express.static(publicPath));
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('disconnect', () => {
+    console.log('User disconected from server');
+  });
+});
 
 var port = process.env.PORT || 3000;
-app.listen(port, () => {
+server.listen(port, () => {
   console.log('SERVER RUNNING ON PORT 3000 (http://localhost:3000)');
 });
