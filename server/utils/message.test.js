f1b63527b2e288f9bcb17dc6ab331e26183b74d9
Bruno Mayer <brunomayer@MacBook-Pro-de-Bruno-Mayer.local>
2016-12-13 18:28:23 -0200

    Add geolocation suport

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,5 +1,5 @@
 var expect = require('expect');
-var {generateMessage} = require('./message')
+var {generateMessage, generateLocationMessage} = require('./message')
 
 describe('generateMessage', () => {
   it('should generate the correct message object', () => {
@@ -7,5 +7,13 @@ describe('generateMessage', () => {
     expect(obj.from).toBe('Bruno');
     expect(obj.text).toBe('This is a message');
     expect(obj.createdAt).toBeA('number');
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location object', () => {
+    var obj = generateLocationMessage('Bruno', '1', '2');
+    expect(obj.url).toBe('https://www.google.com/maps?q=1,2');
+    expect(obj.from).toBe('Bruno');
   });
 });
